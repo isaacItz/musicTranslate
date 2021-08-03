efbf68275a947d43e2b8d70181ce1fd5f69cc321
Isaac Lugo <isaac.est.cbtis@gmail.com>
2021-08-03 12:19:20 -0500

    segundos pasos

--- spTranslator.fish
+++ spTranslator.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/fish
+#set je (mpc -f "%file%" | head -n 1 | sed -r 's/.*\///' | sed -r 's/(\.mp3|\.flac)/.lrc/') ; find /home/lugo/DOWNLOADS/ -iname "*$je*"
 set nombre (mpc -f "%artist% - %title%" | head -n 1)
 set ubicacion /home/lugo/.lyrics/$nombre.txt
 crow -t es -f $ubicacion | tee $nombre.templyc
