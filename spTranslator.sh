a22825afe8564a99a4896c03d22841bcc3ded4ea
Isaac Lugo <isaac.est.cbtis@gmail.com>
2021-08-04 11:37:57 -0500

    it's almost there

--- spTranslator.sh
+++ spTranslator.sh
@@ -1,38 +1,79 @@
 #!/usr/bin/fish
 #set je (mpc -f "%file%" | head -n 1 | sed -r 's/.*\///' | sed -r 's/(\.mp3|\.flac)/.lrc/') ; find /home/lugo/DOWNLOADS/ -iname "*$je*"
-set nombre (mpc -f "%artist% - %title%" | head -n 1)
-set ubicacion /home/lugo/.lyrics/$nombre.txt
-set tempPath letras/$nombre.templyc
+set NOMBRE (mpc -f "%artist% - %title%" | head -n 1)
+set UBICACION /home/lugo/.lyrics/$NOMBRE.txt
+set tempPath letras/$NOMBRE.templyc
 
-function yaModificada -a ruta
-    return (head -n 1 $ruta | grep -Pq '^\-\-\-$')
+function yaModificada
+    return (head -n 1 $UBICACION | grep -Pq '^\-\-\-$')
 end
 
-if test -e $ubicacion
-    if ! yaModificada $ubicacion
-        echo hola
-        #crow -t es -f $ubicacion | tee $tempPath
-        #crow -t es -f $ubicacion
-        cat $ubicacion | crow -t es -i > $tempPath # -f $ubicacion
-        set rutaNueva (env tempPath=$tempPath ./swaper.py)
-        mv $ubicacion $ubicacion.bak
-        cp letras/$rutaNueva $ubicacion
+function findLyrics
+    set CANCIONACTUAL /home/lugo/DOWNLOADS/(mpc -f "%file%" | head -n 1 |  sed -r 's/\.\w{3,4}/.lrc/')
+    if test -e $CANCIONACTUAL
+        cp $UBICACION $UBICACION.bak
+        yes | cp $CANCIONACTUAL $UBICACION
+        return 0
     else
-        echo there is already swaped
+        return 1
     end
-else
-    echo file does no exists reading for lyrics
+end
+
+function swap
+    if test -e $UBICACION
+        if ! yaModificada
+            #crow -t es -f $UBICACION | tee $tempPath
+            #crow -t es -f $UBICACION
+            cat $UBICACION | crow -t es -i > $tempPath # -f $UBICACION
+            set RUTANUEVA (env tempPath=$tempPath ./swaper.py)
+            mv $UBICACION $UBICACION.bak
+            cp letras/$RUTANUEVA $UBICACION
+            return 0
+        else
+            echo there is already swaped
+            return 1
+        end
+    else
+        echo txt does no exists
+        return 1
+    end
+end
+
+function readLocalLyrics
     if test -e letra
-        yes | cp letra $ubicacion
+        yes | cp letra $UBICACION
+    end
+end
+
+function lyrics
+    switch $argv
+    case '--find'
+        if findLyrics
+            swap
+        else
+            echo "there is no lrc file in song's directory"
+        end
+    case --create
+        touch $UBICACION
+        if test $status = 0
+            echo file created
+        end
+        vim $UBICACION
+    case --edit
+        vim $UBICACION
+    case '*'
+        swap
     end
 end
 
 function estaEnEspañol
-   set idiomas (mpc -f "%albumartist% %album% %title%" | head -n 1 | crow -t es -i | grep -P '\[.*\]')
-   echo $idiomas
-   if test $idiomas = '[ Spanish -> Spanish ]'
+   set IDIOMAS (mpc -f "%albumartist% %album% %title%" | head -n 1 | crow -t es -i | grep -P '\[.*\]')
+   echo $IDIOMAS
+   if test $IDIOMAS = '[ Spanish -> Spanish ]'
        return 0
    else
        return 1
    end
 end
+
+lyrics $argv
